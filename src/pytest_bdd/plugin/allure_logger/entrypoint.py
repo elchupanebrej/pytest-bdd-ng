6a991970ddf38b7034ed34738733ec1337cf826e
Kostiantyn Goloveshko <kostyagoloveshko@gmail.com>
2025-07-04 17:29:40 +0300

    Apply allure logger workaround only for older allure versions

--- src/pytest_bdd/plugin/allure_logger/entrypoint.py
+++ src/pytest_bdd/plugin/allure_logger/entrypoint.py
@@ -1,4 +1,5 @@
 from functools import partial
+from operator import ge
 
 import pytest
 from pluggy import HookimplMarker
@@ -6,6 +7,7 @@ from pluggy import HookimplMarker
 from pytest_bdd.compatibility.allure import ALLURE_INSTALLED
 from pytest_bdd.compatibility.pytest import PYTEST81, Config
 from pytest_bdd.plugin.allure_logger.plugin import AllureLogger, PatchedAllureListener
+from pytest_bdd.util.packaging import compare_distribution_version
 from pytest_bdd.util.toolz_extra import flip
 
 if ALLURE_INSTALLED:
@@ -17,8 +19,8 @@ else:
 
 @pytest.mark.trylast
 def pytest_configure(config: Config) -> None:
-    allure_accesible = config.pluginmanager.hasplugin("allure_pytest") and config.option.allure_report_dir
-    if PYTEST81 or not allure_accesible:
+    allure_accessible = config.pluginmanager.hasplugin("allure_pytest") and config.option.allure_report_dir
+    if PYTEST81 or not allure_accessible:
         return
 
     listener = next(
@@ -28,8 +30,11 @@ def pytest_configure(config: Config) -> None:
         ),
     )
 
-    allure_listener = PatchedAllureListener(listener)
-    allure_plugin_manager.register(allure_listener)
+    if compare_distribution_version("allure-python-commons", "2.14.2", ge):
+        allure_listener = listener
+    else:
+        allure_listener = PatchedAllureListener(listener)
+        allure_plugin_manager.register(allure_listener)
 
     allure_logger = AllureLogger(allure_listener)
     config.pluginmanager.register(allure_logger, name=AllureLogger.plugin_name)
