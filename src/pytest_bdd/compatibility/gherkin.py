45c160c3e45cba5e8865ed24bdf52d7c1f4207ca
Kostiantyn Goloveshko <kostyagoloveshko@gmail.com>
2026-04-29 19:00:47 +0000

    chore: type compatibility gherkin filename shim

--- src/pytest_bdd/compatibility/gherkin.py
+++ src/pytest_bdd/compatibility/gherkin.py
@@ -3,3 +3,5 @@ from gherkin.parser_types import GherkinDocument as BaseGherkinDocument
 
 class GherkinDocument(BaseGherkinDocument):
     uri: str | None
+    # TODO: remove all usages of this field
+    _pytest_bdd_filename: str | None
