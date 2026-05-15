8dc3dd7cb5c24730fb166463d179b4a929a0daa7
Kostiantyn Goloveshko <kostyagoloveshko@gmail.com>
2026-05-15 21:47:06 +0300

    feat(07-01): add sphinx>=7.0 to doc-gen extra, enable napoleon in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,13 @@ import pytest_bdd
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinxcontrib.mermaid"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon", "sphinxcontrib.mermaid"]
+
+# Napoleon settings for Google-style docstrings (D-02)
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_use_param = True
+napoleon_use_rtype = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
