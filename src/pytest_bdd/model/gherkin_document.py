d4af734ec708d9489b59b43abf61005d8c6b48d3
Kostiantyn Goloveshko <kostyagoloveshko@gmail.com>
2026-08-28 23:51:20 +0300

    refactor(domain): alias gherkin_document to frozen document model (refs #143, #150, #151)

--- src/pytest_bdd/model/gherkin_document.py
+++ src/pytest_bdd/model/gherkin_document.py
@@ -1,29 +1,5 @@
-"""Feature legacy shim."""
-
 from __future__ import annotations
 
-from typing import cast
+from pytest_bdd.model.document import GherkinDocument
 
-from attr import Factory, attrib, attrs
-
-
-@attrs
-class Feature:
-    gherkin_document = attrib(default=None)
-    uri: str = attrib(default="")
-    filename: str = attrib(default="")
-
-    registry: dict = attrib(default=Factory(dict))
-
-    @property
-    def name(self) -> str | None:
-        if self.gherkin_document and self.gherkin_document.feature is not None:
-            return cast("str", self.gherkin_document.feature.name)
-        return None
-
-    @property
-    def rel_filename(self):
-        file_schema = "file"
-        if self.uri.startswith(file_schema):
-            return self.uri[len(file_schema) + 1 :]
-        return None
+__all__ = ["GherkinDocument"]
