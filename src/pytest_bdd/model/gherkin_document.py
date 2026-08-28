28e7c2e9c1f2c9cde39842c959c46451ee70c617
Kostiantyn Goloveshko <kostyagoloveshko@gmail.com>
2026-08-28 23:48:54 +0300

    refactor(domain): remove legacy step resolution methods from gherkin_document (refs #143, #150, #151)

--- src/pytest_bdd/model/gherkin_document.py
+++ src/pytest_bdd/model/gherkin_document.py
@@ -23,25 +23,22 @@ one line.
 from collections.abc import Sequence
 from itertools import chain
 from textwrap import dedent
-from typing import Union, cast
+from typing import cast
 
 from attr import Factory, attrib, attrs
-from gherkin.errors import CompositeParserException  # type: ignore[import]
-from gherkin.pickles.compiler import Compiler  # type: ignore[import]
 
-from messages import Background, Examples  # type:ignore[attr-defined, import-untyped]
-from messages import Feature as FeatureMessage  # type:ignore[attr-defined]
-from messages import (  # type:ignore[attr-defined, import-untyped]
+from messages import (  # type:ignore[attr-defined, import-untyped]  # type:ignore[attr-defined, import-untyped]
+    Background,
+    Examples,
     GherkinDocument,
-    Location,
     Pickle,
-    PickleStep,
     Rule,
     Scenario,
     Step,
     TableRow,
     Tag,
 )
+from messages import Feature as FeatureMessage  # type:ignore[attr-defined]
 from pytest_bdd.const import TAG_PREFIX
 from pytest_bdd.utils import _itemgetter, deepattrgetter
 
@@ -74,9 +71,7 @@ class Feature:
                     chain(
                         obj.tags,
                         chain.from_iterable(
-                            map(
-                                lambda child: filter(None, [child.rule, child.background, child.scenario]), obj.children
-                            )
+                            filter(None, [child.rule, child.background, child.scenario]) for child in obj.children
                         ),
                     ),
                 )
@@ -90,21 +85,7 @@ class Feature:
                     cls.get_child_ids_gen,
                     chain(
                         obj.tags,
-                        chain.from_iterable(
-                            map(
-                                lambda child: filter(
-                                    None,
-                                    [
-                                        # TODO Check why we don't support nested Rules;
-                                        # https://github.com/cucumber/gherkin/issues/126
-                                        # child.rule,
-                                        child.background,
-                                        child.scenario,
-                                    ],
-                                ),
-                                obj.children,
-                            )
-                        ),
+                        chain.from_iterable(filter(None, [child.background, child.scenario]) for child in obj.children),
                     ),
                 )
             )
@@ -128,27 +109,23 @@ class Feature:
             yield from chain.from_iterable(
                 map(cls.get_child_ids_gen, chain(obj.tags, [obj.table_header], obj.table_body))
             )
-        elif isinstance(obj, TableRow):
-            yield obj.id, obj
-        elif isinstance(obj, Step):
+        elif isinstance(obj, TableRow | Step):
             yield obj.id, obj
 
     load_gherkin_document = staticmethod(GherkinDocument.model_validate)  # type: ignore[attr-defined] # migration to pydantic2
 
     @property
-    def name(self) -> Union[str, None]:
+    def name(self) -> str | None:
         if self.gherkin_document.feature is not None:
-            return cast(str, self.gherkin_document.feature.name)
-        else:
-            return None
+            return cast("str", self.gherkin_document.feature.name)
+        return None
 
     @property
     def rel_filename(self):
         file_schema = "file"
         if self.uri.startswith(file_schema):
             return self.uri[len(file_schema) + 1 :]
-        else:
-            return None
+        return None
 
     @property
     def line_number(self):
@@ -160,59 +137,18 @@ class Feature:
 
     @property
     def tag_names(self):
-        return sorted(map(lambda tag: tag.name.lstrip(TAG_PREFIX), self.gherkin_document.feature.tags))
+        return sorted(tag.name.lstrip(TAG_PREFIX) for tag in self.gherkin_document.feature.tags)
 
     def build_pickle_table_rows_breadcrumb(self, pickle):
-        table_rows_lines = ",".join(
-            map(
-                lambda row: f"line: {deepattrgetter('location.line', default=-1)(row)[0]}",
-                self._get_pickle_ast_table_rows(pickle),
-            )
-        )
+        getter = deepattrgetter("location.line", default=-1)
+        table_rows_lines = ",".join(f"line: {getter(row)[0]}" for row in self._get_pickle_ast_table_rows(pickle))
         return f"[table_rows:[{table_rows_lines}]]" if table_rows_lines else ""
 
     def _get_pickle_ast_table_rows(self, pickle):
-        return list(filter(lambda node: type(node) is TableRow, self._get_linked_ast_nodes(pickle)))
+        return [node for node in self._get_linked_ast_nodes(pickle) if type(node) is TableRow]
 
     def _get_linked_ast_nodes(self, obj):
         return _itemgetter(
             *((obj.ast_node_id,) if hasattr(obj, "ast_node_id") else ()),
             *getattr(obj, "ast_node_ids", ()),
         )(self.registry)
-
-    def _get_pickle_tag_names(self, pickle: Pickle):
-        return sorted(map(lambda tag: tag.name.lstrip(TAG_PREFIX), pickle.tags))  # type: ignore[no-any-return]
-
-    def _get_pickle_ast_scenario(self, pickle: Pickle) -> Scenario:
-        return cast(Scenario, next(filter(lambda node: type(node) is Scenario, self._get_linked_ast_nodes(pickle))))
-
-    def _get_pickle_line_number(self, pickle: Pickle):
-        return (
-            cast(Location, location).line
-            if (location := self._get_pickle_ast_scenario(pickle).location) is not None
-            else -1
-        )
-
-    def _get_pickle_step_model_step(self, pickle_step: PickleStep):
-        return cast(Step, next(filter(lambda node: type(node) is Step, self._get_linked_ast_nodes(pickle_step)), None))
-
-    def _get_step_keyword(self, step: PickleStep):
-        model_step: Union[Step, None] = self._get_pickle_step_model_step(step)
-        if model_step is not None:
-            return model_step.keyword.strip()
-
-    def _get_step_prefix(self, step: PickleStep):
-        step_keyword = self._get_step_keyword(step)
-        if step_keyword is not None:
-            return step_keyword.lower()
-
-    def _get_step_line_number(self, step: PickleStep):
-        model_step: Union[Step, None] = self._get_pickle_step_model_step(step)
-        if model_step is not None:
-            return location.line if (location := model_step.location) is not None else -1
-
-    def _get_step_doc_string(self, step: PickleStep):
-        return getattr(self._get_pickle_step_model_step(step), "doc_string", None)
-
-    def _get_step_data_table(self, step: PickleStep):
-        return getattr(self._get_pickle_step_model_step(step), "data_table", None)
