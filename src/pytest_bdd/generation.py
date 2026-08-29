2928ec2648f06f1d8ab47eff16009bcb10a4f519
Kostiantyn Goloveshko <kostyagoloveshko@gmail.com>
2026-08-29 20:57:10 +0300

    feat(generation): modernize step definition code generation (refs #143, #150, #151)

--- src/pytest_bdd/generation.py
+++ src/pytest_bdd/generation.py
@@ -1,24 +1,31 @@
 """pytest-bdd missing test code generation."""
 
+from __future__ import annotations
+
 import argparse
 import os.path
-from collections.abc import Iterable, Sequence
 from itertools import chain, filterfalse, zip_longest
 from operator import lt, methodcaller
 from pathlib import Path
-from typing import Optional, Union, cast
+from typing import TYPE_CHECKING, cast
 
 import py
 from mako.template import Template
 
-from messages import Pickle, PickleStep  # type:ignore[attr-defined, import-untyped]
 from pytest_bdd.compatibility.importlib.resources import as_file, files
-from pytest_bdd.compatibility.pytest import Config, ExitCode, FixtureRequest, Item, Parser, Session, wrap_session
-from pytest_bdd.model import Feature, StepType
+from pytest_bdd.compatibility.pytest import wrap_session
+from pytest_bdd.model import StepType
 from pytest_bdd.packaging import compare_distribution_version
 from pytest_bdd.parser import GherkinParser
-from pytest_bdd.steps import StepHandler
+from pytest_bdd.steps import Matcher
 from pytest_bdd.utils import make_python_name
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable, Sequence
+
+    from messages import Pickle, PickleStep
+    from pytest_bdd.compatibility.pytest import Config, ExitCode, FixtureRequest, Item, Parser, Session
+    from pytest_bdd.model import Feature
 
 STEP_TYPE_TO_STEP_PREFIX = {
     StepType.unknown: "*",
@@ -36,10 +43,11 @@ STEP_TYPE_TO_STEP_METHOD_NAME = {
 }
 
 
-def check_existense(file_name):
+def check_existense(file_name: str) -> Path:
     """Check file or directory name for existence."""
     if not os.path.exists(file_name):
-        raise argparse.ArgumentTypeError(f"{file_name} is an invalid file or directory name")
+        msg = f"{file_name} is an invalid file or directory name"
+        raise argparse.ArgumentTypeError(msg)
     return Path(file_name)
 
 
@@ -73,14 +81,13 @@ def add_options(parser: Parser) -> None:
     )
 
 
-def cmdline_main(config: Config) -> Optional[int]:
+def cmdline_main(config: Config) -> int | ExitCode | None:
     """Check config option to show missing code."""
     if config.option.generate_missing:
         return generate_and_print_missing_code(config)
-    elif config.option.generate:
+    if config.option.generate:
         return generate_and_print_code(config)
-    else:
-        return None  # Make mypy happy
+    return None
 
 
 def generate_code(
@@ -90,7 +97,7 @@ def generate_code(
 ) -> str:
     """Generate test code for the given filenames."""
     with as_file(files("pytest_bdd.template").joinpath("test.py.mak")) as path:
-        template = Template(filename=str(path))
+        template = Template(filename=str(path))  # noqa: S702
     code = template.render(
         features=features,
         feature_pickles=feature_pickles,
@@ -100,10 +107,10 @@ def generate_code(
         make_string_literal=make_string_literal,
         step_type_to_method_name=STEP_TYPE_TO_STEP_METHOD_NAME,
     )
-    return cast(str, code)
+    return cast("str", code)
 
 
-def generate_and_print_missing_code(config: Config) -> Union[int, ExitCode]:
+def generate_and_print_missing_code(config: Config) -> int | ExitCode:
     """Wrap pytest session to show missing code."""
 
     def _(config: Config, session: Session) -> None:
@@ -125,7 +132,7 @@ def generate_and_print_missing_code(config: Config) -> Union[int, ExitCode]:
             method_name = "prepare" if is_legacy_pytest else "setup"
             methodcaller(method_name, item)(item.session._setupstate)
 
-            item = cast(Item, item)
+            item = cast("Item", item)
             item_request: FixtureRequest = item._request
             pickle: Pickle = item_request.getfixturevalue("scenario")
             feature: Feature = item_request.getfixturevalue("feature")
@@ -138,7 +145,7 @@ def generate_and_print_missing_code(config: Config) -> Union[int, ExitCode]:
                     item_request.config.hook.pytest_bdd_match_step_definition_to_step(
                         request=item_request, feature=feature, scenario=pickle, step=step, previous_step=previous_step
                     )
-                except StepHandler.Matcher.MatchNotFoundError:
+                except Matcher.MatchNotFoundError:
                     non_matched_feature_pickle_steps.append(((feature, pickle), step))
                 finally:
                     previous_step = step
@@ -150,7 +157,7 @@ def generate_and_print_missing_code(config: Config) -> Union[int, ExitCode]:
         features = GherkinParser().get_from_paths(config, list(map(Path, config.option.features)))
 
         seen_features_uris = set()
-        for feature_uri, pickle_name in seen_feature_pickles_ids:
+        for feature_uri, _pickle_name in seen_feature_pickles_ids:
             if feature_uri not in seen_features_uris:
                 seen_features_uris.add(feature_uri)
 
@@ -159,25 +166,22 @@ def generate_and_print_missing_code(config: Config) -> Union[int, ExitCode]:
         non_seen_feature_pickles = list(
             filter(
                 lambda feature_pickle: (feature_pickle[0].uri, feature_pickle[1].name) not in seen_feature_pickles_ids,
-                chain.from_iterable(map(lambda feature: zip_longest((), feature.pickles, fillvalue=feature), features)),
+                chain.from_iterable(zip_longest((), feature.pickles, fillvalue=feature) for feature in features),
             )
         )
 
         unique_step_defs_ids = {(step.type, step.text) for _, step in non_matched_feature_pickle_steps}
-        unique_non_matched_feature_pickle_steps = list(
-            map(
-                lambda step_def_id: next(
-                    filter(
-                        lambda feature_pickle_step: (
-                            feature_pickle_step[1].type == step_def_id[0]
-                            and feature_pickle_step[1].text == step_def_id[1]
-                        ),
-                        non_matched_feature_pickle_steps,
-                    )
-                ),
-                unique_step_defs_ids,
+        unique_non_matched_feature_pickle_steps = [
+            next(
+                filter(
+                    lambda feature_pickle_step, sid=step_def_id: (
+                        feature_pickle_step[1].type == sid[0] and feature_pickle_step[1].text == sid[1]
+                    ),
+                    non_matched_feature_pickle_steps,
+                )
             )
-        )
+            for step_def_id in unique_step_defs_ids
+        ]
 
         print_missing_code(
             non_seen_features,
@@ -192,7 +196,7 @@ def generate_and_print_missing_code(config: Config) -> Union[int, ExitCode]:
     return wrap_session(config=config, doit=_)
 
 
-def generate_and_print_code(config: Config) -> Union[int, ExitCode]:
+def generate_and_print_code(config: Config) -> int | ExitCode:
     """Wrap pytest session to show missing code."""
 
     def _(config: Config, session: Session) -> None:
@@ -208,41 +212,36 @@ def generate_and_print_code(config: Config) -> Union[int, ExitCode]:
 
         feature_pickles: Sequence[tuple[Feature, Pickle]] = list(
             chain.from_iterable(
-                map(
-                    lambda feature: cast(
-                        Iterable[tuple[Feature, Pickle]], zip_longest((), feature.pickles, fillvalue=feature)
-                    ),
-                    features,
+                cast(
+                    "Iterable[tuple[Feature, Pickle]]",
+                    zip_longest((), feature.pickles, fillvalue=feature),
                 )
+                for feature in features
             )
         )
 
         feature_pickles_steps: Sequence[tuple[tuple[Feature, Pickle], PickleStep]] = list(
             chain.from_iterable(
-                map(
-                    lambda feature_pickle: cast(
-                        Iterable[tuple[tuple[Feature, Pickle], PickleStep]],
-                        zip_longest((), feature_pickle[1].steps, fillvalue=feature_pickle),
-                    ),
-                    feature_pickles,
+                cast(
+                    "Iterable[tuple[tuple[Feature, Pickle], PickleStep]]",
+                    zip_longest((), feature_pickle[1].steps, fillvalue=feature_pickle),
                 )
+                for feature_pickle in feature_pickles
             )
         )
 
         unique_step_defs_ids = {(step.type, step.text) for (feature, pickle), step in feature_pickles_steps}
         unique_feature_pickle_steps = sorted(
-            list(
-                map(
-                    lambda step_def_id: next(  # type: ignore[no-any-return]
-                        filter(
-                            lambda s: (s[1].type == step_def_id[0] and s[1].text == step_def_id[1]),
-                            feature_pickles_steps,
-                        )
-                    ),
-                    unique_step_defs_ids,
+            [
+                next(
+                    filter(
+                        lambda s, sid=step_def_id: s[1].type == sid[0] and s[1].text == sid[1],
+                        feature_pickles_steps,
+                    )
                 )
-            ),
-            key=lambda feature_pickle_step: cast(str, feature_pickle_step[1].text),
+                for step_def_id in unique_step_defs_ids
+            ],
+            key=lambda feature_pickle_step: cast("str", feature_pickle_step[1].text),
         )
 
         code = generate_code(features, feature_pickles, unique_feature_pickle_steps)
@@ -259,10 +258,10 @@ def generate_and_print_code(config: Config) -> Union[int, ExitCode]:
 
 
 def print_missing_code(
-    features,
+    features: Sequence[Feature],
     feature_pickles: Sequence[tuple[Feature, Pickle]],
     feature_pickle_steps: Sequence[tuple[tuple[Feature, Pickle], PickleStep]],
-    unique_steps,
+    unique_steps: Sequence[tuple[tuple[Feature, Pickle], PickleStep]],
 ) -> None:
     """Print missing code with TerminalWriter."""
     tw = py.io.TerminalWriter()
@@ -283,9 +282,8 @@ def print_missing_code(
         tw.line()
         step_type = STEP_TYPE_TO_STEP_PREFIX[step.type if step.type is not None else StepType.unknown]
         tw.line(
-            f"""StepHandler {step_type} "{step.text}" is not defined in the scenario "{pickle.name}" in the feature"""
-            f""" "{feature.name}" in the file"""
-            f""" {feature.filename}:{feature._get_step_line_number(step)}""",
+            f'StepHandler {step_type} "{step.text}" is not defined in the scenario "{pickle.name}" in the feature'
+            f' "{feature.name}" in the file {feature.filename}:{feature._get_step_line_number(step)}',
             red=True,
         )
 
@@ -301,9 +299,11 @@ def print_missing_code(
 
 def make_python_docstring(string: str) -> str:
     """Make a python docstring literal out of a given string."""
-    return '"""{}."""'.format(string.replace('"""', '\\"\\"\\"'))
+    escaped = string.replace('"""', '\\"\\"\\"')
+    return f'"""{escaped}."""'
 
 
 def make_string_literal(string: str) -> str:
     """Make python string literal out of a given string."""
-    return "'{}'".format(string.replace("'", "\\'"))
+    escaped = string.replace("'", "\\'")
+    return f"'{escaped}'"
